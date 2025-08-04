ab79a9280db4c922970b62b3c410add306cd016d
anfigeno <dolorcriticodevastador@proton.me>
2025-08-04 13:56:00 -0500

    Más ventanas flotantes

--- modulosHomeManager/hyprland/config/reglasDeVentana.nix
+++ modulosHomeManager/hyprland/config/reglasDeVentana.nix
@@ -1,15 +1,27 @@
 { lib }:
 let
   ventanasFlotantes = {
-    porNombreYClase = [{
-      clase = "floorp";
-      titulo = "Floorp - Choose User Profile";
-    }];
+    porNombreYClase = [
+      {
+        clase = "floorp";
+        titulo = "Floorp - Choose User Profile";
+      }
+      {
+        clase = "xdg-desktop-portal-gtk";
+        titulo = "Save As";
+      }
+      {
+        clase = "zen";
+        titulo = "Zen - Choose User Profile";
+      }
+    ];
     porClase = [ "io.bassi.Amberol" ];
   };
-in lib.mkMerge [
-  (lib.map (ventana: "float, class:${ventana.clase}, title:${ventana.titulo}")
-    ventanasFlotantes.porNombreYClase)
+in
+lib.mkMerge [
+  (lib.map (
+    ventana: "float, class:${ventana.clase}, title:${ventana.titulo}"
+  ) ventanasFlotantes.porNombreYClase)
   (lib.map (clase: "float, class:${clase}") ventanasFlotantes.porClase)
   [ "size 200 400, class:^(io.bassi.Amberol)$" ]
 ]
