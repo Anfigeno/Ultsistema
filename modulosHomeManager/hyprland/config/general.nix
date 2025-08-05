49a68a1dc2f56a2adf87c27d2ab84c5e61afe72a
anfigeno <dolorcriticodevastador@proton.me>
2025-08-04 21:37:14 -0500

    Cambiamos el tema de hyprland para adaptarlo a Orchis

--- modulosHomeManager/hyprland/config/general.nix
+++ modulosHomeManager/hyprland/config/general.nix
@@ -1,7 +1,10 @@
+let
+  orchis = import ../../../lib/orchis.nix;
+in
 {
   border_size = 1;
-  "col.active_border" = "rgba(313749ff)";
-  "col.inactive_border" = "rgba(313749ff)";
+  "col.active_border" = "rgb(${builtins.substring 1 (-1) orchis.grey."600"})";
+  "col.inactive_border" = "rgb(${builtins.substring 1 (-1) orchis.grey."600"})";
   gaps_in = 10;
   gaps_out = 10;
 }
