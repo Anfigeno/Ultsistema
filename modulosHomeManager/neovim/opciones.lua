6a662da4e884eb7b3efa80a1c21575b856d60d0b
anfigeno <dolorcriticodevastador@proton.me>
2025-09-20 15:14:53 -0500

    Autocomando para resolver un problemita con el formateo de json

--- modulosHomeManager/neovim/opciones.lua
+++ modulosHomeManager/neovim/opciones.lua
@@ -16,4 +16,11 @@ vim.api.nvim_create_user_command("Hints", function()
 	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, {})
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json" },
+	callback = function()
+		vim.api.nvim_set_option_value("formatprg", "jq", { scope = "local" })
+	end,
+})
+
 vim.keymap.set("n", "<c-s>", "<cmd>w<cr>")
