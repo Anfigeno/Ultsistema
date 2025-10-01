b76c8fafdb402a96adc7d0df9ebb517b15f231c7
anfigeno <dolorcriticodevastador@proton.me>
2025-09-30 19:08:12 -0500

    Instalamos keep-split-ratio

--- modulosHomeManager/neovim/default.nix
+++ modulosHomeManager/neovim/default.nix
@@ -92,6 +92,12 @@ let
     ref = "main";
     repo = "oribarilan/lensline.nvim";
   };
+
+  propio-keep-split-ratio-nvim = deGithub {
+    rev = "906aa5196e194dca12c7729069760d42e77ddcff";
+    ref = "main";
+    repo = "adlrwbr/keep-split-ratio.nvim";
+  };
 in
 {
   options.modulosHomeManager.neovim = {
@@ -110,6 +116,13 @@ in
         curl
       ];
       plugins = with pkgs.vimPlugins; [
+        {
+          plugin = propio-keep-split-ratio-nvim;
+          type = "lua";
+          config = # lua
+            ''require("keep-split-ratio").setup()'';
+        }
+
         {
           plugin = propio-lensline-nvim;
           type = "lua";
