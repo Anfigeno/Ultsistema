3dbf8eb39fbdd0ebf603461320176cf3dc32f4bd
anfigeno <dolorcriticodevastador@proton.me>
2025-11-02 17:14:42 -0500

    Configuración de mimeapps

--- modulosHomeManager/xdg/default.nix
+++ modulosHomeManager/xdg/default.nix
@@ -94,13 +94,7 @@ in
         };
       };
 
-      mimeApps = {
-        enable = true;
-        defaultApplications = {
-          "inode/directory" = "org.gnome.Nautilus.desktop";
-          "application/x-gnome-saved-search" = "org.gnome.Nautilus.desktop";
-        };
-      };
+      mimeApps = import ./config/mimeApps.nix;
     };
   };
 }
